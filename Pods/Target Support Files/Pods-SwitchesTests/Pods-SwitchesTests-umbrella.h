9ea309f24bb0630fc505db104e15c848710c91f4
Ahmed Musse <amusse@princeton.edu>
2016-05-31 22:34:44 -0500

    Removed iphone portion

--- Pods/Target Support Files/Pods-SwitchesTests/Pods-SwitchesTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SwitchesTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwitchesTestsVersionString[];
-
--- Pods/Target Support Files/Pods-SwitchesUITests/Pods-SwitchesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwitchesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwitchesUITests
-@end
